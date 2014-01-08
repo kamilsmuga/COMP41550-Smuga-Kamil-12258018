f4e9f42cd1d3d6cc5ddec4fe6f25f900796829da
Kamil Smuga <smugakamil@gmail.com>
2014-01-08 10:29:17 +0000

    part of an initial commit.

---
+++ HelloUCD/HelloUCD/HelloUCDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HelloUCDAppDelegate.h
+//  HelloUCD
+//
+//  Created by Kamil Smuga on 08/01/2014.
+//  Copyright (c) 2014 COMP41550. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloUCDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
